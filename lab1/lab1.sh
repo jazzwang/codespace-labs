33a2858f0217424c748f814d554802687dfcb989
jazzwang <jazzwang.tw@gmail.com>
2025-11-10 11:01:34 +0000

    docs: [lab1] add README.md

--- lab1/lab1.sh
+++ lab1/lab1.sh
@@ -5,9 +5,10 @@ export DISPLAY=:1
 # Install essential packages
 apt-get update
 apt-get install -y --no-install-recommends \
-    xvfb \
-    fluxbox \
-    dbus-x11 \
+    xvfb      \
+    fluxbox   \
+    x11vnc    \
+    dbus-x11  \
     net-tools \
     novnc
 apt-get clean
